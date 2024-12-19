3eb8797071d73e42ac4a98f165c2c2dd0e6df625
rahul-b-p <rahulzantoz10@gmail.com>
2024-12-19 10:06:20 +0530

    secondary commit

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = require("dotenv");
+const logger_1 = require("./utils/logger");
+(0, dotenv_1.config)();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    logger_1.logger.info(`Server running at http://localhost:${port}`);
+});
